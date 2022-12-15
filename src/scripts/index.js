2204993ec367ba8d00ddee3da24aa55dc1e75ea9
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-12-15 09:40:59 -0600

    Initialize Project Setup

---
+++ src/scripts/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*--------------------------------------------------------------------------- *
+ * Group Think
+ * Jodi-Ann Barrett
+ * 
+ * */
+
+// Utility Functions 
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+// Select HTML element by class, id and html element
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+// function print(arg) {
+//   console.log(arg);
+// }
+
+/**--------------------------------------------------------------------------- */
+
