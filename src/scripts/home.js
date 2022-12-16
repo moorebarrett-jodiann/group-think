65a9f22bcf330a76649e8489a838c1fa47e39e6f
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-12-15 20:38:07 -0600

    Complete users column

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -20,4 +20,63 @@ function select(selector, parent = document) {
 
 /**--------------------------------- Data ------------------------------------- */
 
+const url = 'https://randomuser.me/api/?nat=CA&results=10';
+const userContainer = select('.row .card .users');
+const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    },
+    mode: 'cors'
+};
+
+/**---------------------------------------------------------------------------- */
+
+/**---------------- Login Validation and Redirect ----------------------------- */
+
+async function getUsers() {
+    try {
+        const result = await fetch(url, options);
+        if(result.status >= 200 && result.status < 400) {
+            const users = await result.json();
+            printUsers(users.results);
+        }
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+// function to build user cards
+function printUsers(users) {
+    users.forEach(user => {
+        var div = document.createElement('div');
+        const picture = user.picture?.medium;
+        const name = `${user.name?.first} ${user.name?.last}`;
+        const city = user.location?.city;
+        const country = user.location?.country;
+
+        div.classList.add('user-cards');
+        div.innerHTML = `
+            <div class="user-card flexbox">
+                <div class="img">
+                    <img src=${picture}>
+                </div>
+                <div class="details">
+                    <p class="name">${name}</p>
+                    <p class="city">${city}, ${country}</p>
+                </div>
+                <div class="connect">
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+            </div>
+        `;
+        userContainer.appendChild(div);
+    });
+}
+
+// when page is loaded populate grid
+onEvent('load', window, () => {
+    getUsers();
+});
+
 /**---------------------------------------------------------------------------- */
